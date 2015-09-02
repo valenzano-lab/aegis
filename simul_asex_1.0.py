3db380537b1469ee56237502fc2a1a3e65d71ac6
William John Bradshaw <wbradshaw@age.mpg.de>
2015-09-02 14:03:50 +0200

    Separated functions into separate file

--- simul_asex_1.0.py
+++ simul_asex_1.0.py
@@ -7,7 +7,7 @@ import cPickle
 import scipy.stats as stats
 import copy
 import itertools
-
+import simul_functions
 ## prompts
 #res_prompt = raw_input('\nResources as a function of population[pop] or a constant[const]? ')
 #if res_prompt=='pop':
@@ -84,36 +84,6 @@ snapshot_plot_stages = np.around(np.linspace(0,number_of_stages,16),0)
 ipx = np.arange(0,71)
 surv_fit_var = np.linspace(0,1,21)
 repr_fit_var = np.linspace(0,1,21)
-
-###############
-## FUNCTIONS ##
-###############
-
-def chance(z):
-    z = round(z * 1000000, 0)
-    if (randint(1, 1000000) <= z):
-        y = True
-    else:
-        y = False
-    return y
-
-def starting_genome(var,n):
-    ### Returns a binary array of length n, with the proportion of 1's
-    ### determined by a truncated normal distribution with variance var
-    var=min(1.4, var)
-    sd = var**0.5
-    p=stats.truncnorm(-0.5/sd, 0.5/sd, loc=0.5, scale=sd).rvs(1) 
-    # Normal distribution with mean 0.5 and sd as given, truncated to between 0 and 1.
-    a = (stats.uniform(0,1).rvs(n)<p)*1
-    return a
-
-def find_nearest(array,value):
-    idx = (np.abs(array-value)).argmin()
-    return idx
-
-def my_shuffle(array):
-    shuffle(array)
-    return array
 
 # Write info file.
 time_print =  time.strftime('%X %x', time.gmtime())
