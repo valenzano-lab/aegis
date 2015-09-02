ff387b25e94cbf7e961dcf73e2115073083ee953
William John Bradshaw <wbradshaw@age.mpg.de>
2015-09-02 15:56:41 +0200

    Changed change() to simple comparison against a uniform.

--- simul_functions.py
+++ simul_functions.py
@@ -1,23 +1,18 @@
-###############
-## FUNCTIONS ##
-###############
+import scipy.stats
+
+rand = scipy.stats.uniform(0,1) # Generate random number generator
 
 def chance(z):
-    z = round(z * 1000000, 0)
-    if (randint(1, 1000000) <= z):
-        y = True
-    else:
-        y = False
-    return y
+    return rand.rvs(1)<=z
 
 def starting_genome(var,n):
     ### Returns a binary array of length n, with the proportion of 1's
     ### determined by a truncated normal distribution with variance var
     var=min(1.4, var)
     sd = var**0.5
-    p=stats.truncnorm(-0.5/sd, 0.5/sd, loc=0.5, scale=sd).rvs(1) 
+    p=scipy.stats.truncnorm(-0.5/sd, 0.5/sd, loc=0.5, scale=sd).rvs(1) 
     # Normal distribution with mean 0.5 and sd as given, truncated to between 0 and 1.
-    a = (stats.uniform(0,1).rvs(n)<p)*1
+    a = (rand.rvs(n)<=p)*1
     return a
 
 def find_nearest(array,value):
