951d917804f9573f28eec39668348645bd4e7c1f
William John Bradshaw <wbradshaw@age.mpg.de>
2015-09-03 12:41:30 +0200

    simul_prompts.py

---
+++ simul_prompts.py
@@ -0,0 +1,31 @@
+## prompts
+#res_prompt = raw_input('\nResources as a function of population[pop] or a constant[const]? ')
+#if res_prompt=='pop':
+#    R = int(raw_input('Resource increment per stage: '))
+#    k_var = float(raw_input('Resource regrowth factor: '))
+#    res_upper_limit = int(raw_input('Resource upper limit: '))
+#    start_res = int(raw_input('Starting amount of resources: '))
+#else:
+#    start_res = int(raw_input('Constant resources value: ')
+#start_age_var = raw_input('All individuals start at reproduction age[y] or age is randomly distributed[random]? ')
+#variance_var = float(raw_input('Variance index[0-1.4]: '))
+#start_pop = int(raw_input('Starting number of individuals: '))
+#number_of_stages = int(raw_input('Number of stages: '))
+#crisis_stages = raw_input('Stages of crisis occurance[separate with a comma]: ')
+#if crisis_stages!='':
+#   crisis_stages = np.array(map(int,crisis_stages.split(',')))
+#else:
+#   crisis_stages = np.array([-1])
+#sample_var = raw_input('Fraction of crisis survivors[0-1]: ')
+#if sample_var!='':
+#   sample_var = float(sample_var)
+#max_death_rate = float(raw_input('Maximal death rate [default: 0.02]: '))
+#surv_rate_distr = raw_input('Initial survival rate (distribution)[random or value in %]: ')
+#repr_rate_distr = raw_input('Initial reproduction rate (distribution)[random or value in %]: ')
+#max_repr_rate = float(raw_input('Maximal reproduction rate [default: 0.4]: '))
+#recomb_rate_var = float(raw_input('Recombination rate [default: 0.01]: '))
+#mut_rate_var = float(raw_input('Mutation rate [default: 0.001]: '))
+#number_of_runs = int(raw_input('Number of runs: '))
+#comment = raw_input('Comment: ')
+#out = raw_input('\nWhat is the path to the output file?\n')
+
