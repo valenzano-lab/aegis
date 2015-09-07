c90af5185a171a99572c5e506ec832103f0b798e
William John Bradshaw <wbradshaw@age.mpg.de>
2015-09-07 17:29:32 +0200

    Working on data logging (still a mess right now)

--- simul_monitoring.py
+++ simul_monitoring.py
@@ -1,48 +1,64 @@
-def some_report_function(population, N, gen_map, chr_len, n_bases, ...):
-            b = n_bases
-            # Initialise output variables:
-            density_surv = np.zeros((21,)) # Survival genotype sum distr
-            density_repr = np.zeros((21,)) # Reprodv genotype sum distr
-            
-            repr_rate_out = np.zeros((71,))
-            repr_rate_junk_out = np.zeros((1,))
-            death_rate_out = np.zeros((71,))
-            death_rate_junk_out = np.zeros((1,))
-            # sd-collect genome values for every age
-            repr_rate_sd = [[]]*71
-            surv_rate_sd = [[]]*71
-            repr_fit = np.zeros((71,))
-            repr_fit_junk = np.zeros((1,))
-            surv_fit = np.zeros((71,))
-            surv_fit_junk = np.zeros((1,))
-            hetrz_mea = np.zeros((1260,)) # heterozigosity measure
-            hetrz_mea_sd = [[]]*1260 # heterozigosity measure sd
+def some_report_function(population, N, gen_map, chr_len, n_bases, 
+        d_range, r_range, max_ls):
+    # Determine if population is already sorted; if not, sort it by age:
+    ages = population[:,0]
+    if ages != ages[ages.argsort()]:
+        population = population[ages.argsort()]
+    b = n_bases # Binary units per locus
 
+    ## INITIALISE OUTPUT VARIABLES ##
 
-            neut_locus = np.nonzero(gen_map==201)[0][0] 
-            neut_pos = np.arange(neut_locus*b, (neut_locus+1)*b)+1
-            for p in range(N):
-                # Get positions of loci (survival, reproductive, neutral):
-                a = population[p]
-                age = a[0]
-                surv_locus = np.nonzero(gen_map==age)[0][0]
-                repr_locus = np.nonzero(gen_map==(age+100))[0][0]
-                surv_pos = np.arange(surv_locus*b, (surv_locus+1)*b)+1
-                repr_pos = np.arange(repr_locus*b, (repr_locus+1)*b)+1
-                # Get genotype sums:
-                surv_gen = sum(a[np.append(surv_pos, surv_pos+chr_len)
-                repr_gen = sum(a[np.append(repr_pos, repr_pos+chr_len)
-                neut_gen = sum(a[np.append(neut_pos, neut_pos+chr_len)
+    # Genotype sum distributions:
+    density_surv = np.zeros((21,))
+    density_repr = np.zeros((21,))
+    # Mean death/repr rates by age:
+    death_rate_out = np.zeros(max_ls)
+    repr_rate_out = np.zeros(max_ls)
+    # Death/repr rate SD by age:
+    surv_rate_sd = np.zeros(max_ls)
+    repr_rate_sd = np.zeros(max_ls)
+    # "Fit"?
+    surv_fit = np.zeros((71,))
+    repr_fit = np.zeros((71,))
+    # "Junk"?
+    repr_rate_junk_out = np.zeros((1,))
+    death_rate_junk_out = np.zeros((1,))
+    repr_fit_junk = np.zeros((1,))
+    surv_fit_junk = np.zeros((1,))
+    # Hetrz?
+    hetrz_mea = np.zeros((1260,)) # heterozigosity measure
+    hetrz_mea_sd = [[]]*1260 # heterozigosity measure sd
 
+    neut_locus = np.nonzero(gen_map==201)[0][0] 
+    neut_pos = np.arange(neut_locus*b, (neut_locus+1)*b)+1
+    for x in range(max(ages)):
+        pop = population[population[,0]==x]
+        # Find loci and binary units:
+        surv_locus = np.nonzero(gen_map==age)[0][0]
+        surv_pos = np.arange(surv_locus*b, (surv_locus+1)*b)+1
+        # Subset array to relevant columns and find genotypes:
+        surv_pop = pop[np.append(surv_pos, surv_pos+chr_len)]
+        surv_gen = np.sum(surv_pop, axis=0)
+        # Find death/reproduction rates:
+        death_rates = d_range[surv_gen]
+        # Calculate statistics:
+        death_rate_out[x] = np.mean(death_rates)
+        death_rate_sd[x] = np.std(death_rates)
+        surv_fit[x] = ??
+        if x>=maturity:
+            # Same for reproduction if they're adults
+            repr_locus = np.nonzero(gen_map==(age+100))[0][0]
+            repr_pos = np.arange(repr_locus*b, (repr_locus+1)*b)+1
+            repr_pop = pop[np.append(repr_pos, repr_pos+chr_len)]
+            repr_gen = np.sum(repr_pop, axis=0)
+            repr_rates = r_range[repr_gen]
+            repr_rate_out[x] = np.mean(repr_rates)
+            repr_rate_sd[x] = np.std(repr_rates)
+            repr_fit[x] = ??
+        # Junk variables??
 
-
-
-        if n_stage in np.linspace(0,number_of_stages,16).astype(int):
-            ## reseting output variables
             
                 # Survival statistics:
-                density_surv[surv_out] += 1
-                death_rate_out[surv_locus] += death_rate_var[surv_out]
                 surv_rate_sd[surv_locus].append(1-death_rate_var[surv_out])
                 surv_fit[surv_locus] += surv_fit_var[surv_out]
                 for t in range(surv_pos[0], surv_pos[1]):
@@ -50,7 +66,6 @@ def some_report_function(population, N, gen_map, chr_len, n_bases, ...):
                     hetrz_mea[t] += hetrz
                     hetrz_mea_sd[t].append(hetrz)
                 # Reproduction statistics:
-                density_repr[repr_out] += 1
                 repr_rate_out[repr_locus] += repr_rate_var[repr_out]
                 repr_rate_sd[repr_locus].append(repr_rate_var[repr_out])
                 repr_fit[repr_locus] += repr_fit_var[repr_out]
