6a34f946d0d8c0194c8d490d2a87064e93a0c248
William John Bradshaw <wbradshaw@age.mpg.de>
2015-09-14 13:24:40 +0200

    Moved verbosity into command line args.

--- genome_simulation.py
+++ genome_simulation.py
@@ -58,11 +58,12 @@ for n_run in range(1, c.number_of_runs+1):
     for n_stage in range(c.number_of_stages):
 
         N = len(population)
-        
+        full_report = n_stage%args.r == 0 and args.verbose
+
         if(N==0):
             print "\nPerished at stage "+str(n_stage+1)+"."
             break
-        elif c.verbose or n_stage%10==0:
+        elif n_stage%args.r == 0:
             print "\nStage "+str(n_stage+1)+": "+str(N)+" individuals."
         
         # Record output variables
@@ -79,28 +80,29 @@ for n_run in range(1, c.number_of_runs+1):
         # Change in resources
         if c.res_var: # function of population
             resources = fn.update_resources(resources, N, c.R, c.V, 
-                    c.res_limit, c.verbose)
+                    c.res_limit, full_report)
             x = x*c.death_inc if resources == 0 else 1.0
         else: # constant; death rate increases if population exceeds
             x = x*c.death_inc if N>resources else 1.0
-        if c.verbose: print "Starvation factor: "+str(x)
+        if full_report: print "Starvation factor: "+str(x)
 
         # Reproduction
-        population = fn.reproduction_asex(population, c.maturity, c.max_ls,
-                c.gen_map, c.chr_len, c.r_range, c.m_rate, c.verbose)
+        population = fn.reproduction_asex(population, c.maturity, 
+                c.max_ls, c.gen_map, c.chr_len, c.r_range, c.m_rate, 
+                full_report)
         N = len(population)
 
         # Death
         population = fn.death(population, c.max_ls, c.gen_map, c.chr_len,
-                c.d_range, x, c.verbose)
+                c.d_range, x, full_report)
 
         # Extrinsic death crisis:
         if n_stage in c.crisis_stages:
             N = len(population)
             n_survivors = int(N*c.crisis_sv)
             population = population[sample(range(N), n_survivors)]
-            if c.verbose:
-                print "Crisis! "+str(n.survivors)+" individuals survived."
+            print "Stage " + n_stage + ": Crisis! " + str(n.survivors),
+            print " individuals survived."
 
     ## RUN ENDED
     print "\nEnd of run "+str(n_run)+".\n"
