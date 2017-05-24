acd98c160ec154eaeb64934a6e50c15a007f3a71
William John Bradshaw <wbradshaw@age.mpg.de>
2017-05-24 18:30:16 +0100

    Finished Core.Config class and added tests at Core.Test.test_Config

--- aegis/aegis/Core/Config.py
+++ aegis/aegis/Core/Config.py
@@ -17,78 +17,101 @@ class Config:
     def __init__(self, c):
         """Import basic attributes from a config-file object and set data
         descriptors."""
-        def mirror(self, name, infostring):
+        # First check that maturity, max_ls anh offset values are compatible
+        def mirror(name, infostring):
             """Set a data element's value from the attribute of the same name
             in c, while also setting its description."""
             self.put(name, getattr(c,name), infostring)
-        self.info_dir = {} # Dictionary for element information
-        # Set data elements
-        self.mirror("repr_mode", "The reproductive mode of the population:\
+        self.info_dict = {} # Dictionary for element information
+        ## Set data elements and information ##
+        # Reproductive mode
+        mirror("repr_mode", "The reproductive mode of the population:\
                 'sexual' (recombination and assortment), 'recombine_only',\
                 'assort_only', or 'asexual' (no recombination or assortment).\
-                [string]")
-        self.mirror("number_of_runs", "The number of runs to be \
+                [str]") # Test that this is a valid value
+        # Run parameters
+        mirror("number_of_runs", "The number of runs to be \
             executed in parallel to generate summary data. [int]")
-        self.mirror("number_of_stages", "The number of stages per run. [int]")
-        self.mirror("number_of_snapshots", "The number of stages in the run\
+        mirror("number_of_stages", "The number of stages per run. [int]")
+        mirror("number_of_snapshots", "The number of stages in the run\
                 at which to save detailed data (evenly distributed). [int]")
-        self.mirror("res_start", "The resource level at stage 0. [int]")
-        self.mirror("res_var", "Whether resources vary through consumption\
-                and regrowth (True) or remain constant (False). [bool]")
-        self.mirror("res_limit", "Maximum resource level (-1 = infinite).\
-                [int]") #!
-        self.mirror("R", "Arithmetic regrowth increment under variable\
-                resources. [int]")
-        self.mirror("V", "Geometric regrowth rate under variable\
-                resources. [float]")
-        self.mirror("start_pop", "Population size at stage 0. [int]")
-        self.mirror("g_dist_s", "Initial proportion of 1's in survival loci\
-                at stage 0. [float]")
-        self.mirror("g_dist_r", "Initial proportion of 1's in reproduction\
-                loci at stage 0. [float]")
-        self.mirror("g_dist_n", "Initial proportion of 1's in neutral loci\
-                at stage 0. [float]")
-        self.mirror("death_bound", "Min and max death rates. [float array]")
-        self.mirror("repr_bound", "Min and max reproduction rates.\
+        mirror("start_pop", "Population size at stage 0. [int]")
+        # Death and reproduction
+        mirror("death_bound", "Min and max death rates. [float array]")
+        mirror("repr_bound", "Min and max reproduction rates.\
             [float array]")
-        self.mirror("r_rate", "Per-bit recombination rate, if applicable. \
+        mirror("r_rate", "Per-bit recombination rate, if applicable. \
                 [float]")
-        self.mirror("m_rate", "Per-bit mutation rate during reproduction. \
+        mirror("m_rate", "Per-bit mutation rate during reproduction. \
                 [float]")
-        self.mirror("m_ratio", "The ratio of positive (0 -> 1) to negative\
+        mirror("m_ratio", "The ratio of positive (0 -> 1) to negative\
                 (1 -> 0) mutations during reproduction. Typically negative.\
                 [float]")
-        self.mirror("max_ls", "Maximum lifespan for the population in stages\
-                (-1 = indefinite). [int]") #!
-        self.mirror("maturity", "Age of reproductive maturation in stages;\
-                before this, P(reproduction) = 0. [int]")
-        self.mirror("n_neutral", "Number of neutral loci per genome;\
+        # Genome structure
+        mirror("g_dist_s", "Initial proportion of 1's in survival loci\
+                at stage 0. [float]")
+        mirror("g_dist_r", "Initial proportion of 1's in reproduction\
+                loci at stage 0. [float]")
+        mirror("g_dist_n", "Initial proportion of 1's in neutral loci\
+                at stage 0. [float]")
+        mirror("n_base", "Number of bits per locus in the genome. [int]")
+        mirror("n_neutral", "Number of neutral loci per genome;\
                 more loci reduces the effect of linkage at any given one.\
                 [int]")
-        self.mirror("n_base", "Number of bits per locus in the genome. [int]")
-        self.mirror("surv_pen", "Whether to penalise survival under \
-                starvation by multiplying the death rate. [bool]")
-        self.mirror("repr_pen", "Whether to penalise reproduction under \
-                starvation by dividing the reproduction rate. [bool]")
-        self.mirror("death_inc", "Factor for multiplying death rate of \
-                individuals under starvation. [float]")
-        self.mirror("repr_dec", "Factor for dividing reproduction rate of \
-                individuals under starvation. [float]")
-        self.mirror("windows", "Width of sliding windows for recording \
-                along-genome variation in bit value and along-run \
-                variation in population size and resource levels.")
-        self.mirror("repr_offset", "Number by which to offset reproductive\
+        mirror("repr_offset", "Number by which to offset reproductive\
                 loci in genome map (must > maximum lifespan). [int]")
-        self.mirror("neut_offset", "Number by which to offset neutral loci\
+        mirror("neut_offset", "Number by which to offset neutral loci\
                 in genome map (must > repr_offset + max lifespan - maturity).\
                 [int]")
+        # Resources and starvation
+        mirror("res_start", "The resource level at stage 0. [int]")
+        mirror("res_var", "Whether resources vary through consumption\
+                and regrowth (True) or remain constant (False). [bool]")
+        mirror("res_limit", "Maximum resource level (-1 = infinite).\
+                [int]") #!
+        mirror("R", "Arithmetic regrowth increment under variable\
+                resources. [int]")
+        mirror("V", "Geometric regrowth rate under variable\
+                resources. [float]")
+        mirror("surv_pen", "Whether to penalise survival under \
+                starvation by multiplying the death rate. [bool]")
+        mirror("repr_pen", "Whether to penalise reproduction under \
+                starvation by dividing the reproduction rate. [bool]")
+        mirror("death_inc", "Factor for multiplying death rate of \
+                individuals under starvation. [float]")
+        mirror("repr_dec", "Factor for dividing reproduction rate of \
+                individuals under starvation. [float]")
+        # Life history
+        mirror("max_ls", "Maximum lifespan for the population in stages\
+                (-1 = indefinite). [int]") #!
+        mirror("maturity", "Age of reproductive maturation in stages;\
+                before this, P(reproduction) = 0. [int]")
+        # Sliding windows
+        mirror("windows", "Width of sliding windows for recording \
+                along-genome variation in bit value and along-run \
+                variation in population size and resource levels. [dict]")
+        self.check()
+
+    # Check for invalid config construction
+    def check(self):
+        """Confirm that population parameters are compatible before
+        starting simulation."""
+        if self.maturity > self.max_ls - 2:
+            s = "Age of maturity must be at least 2 less than max lifespan."
+            raise ValueError(s)
+        if self.repr_offset < self.max_ls:
+            s = "Offset for reproductive loci must be >= max lifespan."
+            raise ValueError(s)
+        if self.neut_offset - self.repr_offset < self.max_ls:
+            s = "Difference between offset values must be >= max lifespan."
+            raise ValueError(s)
 
     # Constructors and selectors
 
     def put(self, name, value, infostring):
         """Set a data element's value and description simultaneously."""
         setattr(self, name, value)
-        self.info_dir[name] = infostring
+        self.info_dict[name] = infostring
 
     def get_value(self, key):
         """Get the value of an attribute (equivalent to calling that attribute
@@ -97,7 +120,7 @@ class Config:
 
     def get_info(self, key):
         """Get the description accompanying an attribute."""
-        return self.info_dir[key]
+        return self.info_dict[key]
 
     # Generate derived attributes
 
@@ -105,6 +128,8 @@ class Config:
         """Generate derived configuration attributes from simple ones and
         add to configuration object."""
         # Compute values
+        self.check()
+        # Genome structure
         g_dist_dict = { # Dictionary of initial proportion of 1's in genome loci
                 "s":self.g_dist_s, # Survival
                 "r":self.g_dist_r, # Reproduction
@@ -113,10 +138,10 @@ class Config:
         self.put("g_dist", g_dist_dict, "Dictionary of values specifying\
                 initial proportion of 1's in each locus type (s/r/n) during\
                 population initialisation. [dict of floats]")
-        genmap = np.asarray(range(0,self.max_ls) +\
-                range(self.maturity+self.repr_offset,
-                    self.max_ls+self.repr_offset)+\
-                range(self.neut_offset, self.neut_offset+self.n_neutral))
+        genmap = np.concatenate([
+            np.arange(self.max_ls),
+            np.arange(self.maturity,self.max_ls) + self.repr_offset,
+            np.arange(self.n_neutral) + self.neut_offset], 0)
         self.put("genmap", genmap, "Genome map specifying the relative \
                 position of each survival, reproduction and neutral locus \
                 in the genome of each individual. [int array]")
@@ -128,12 +153,19 @@ class Config:
                 chromosome in bits. [int]")
         self.put("n_states", 2*self.n_base+1, "Number of possible genotype \
                 states (from 0 to 2*n_base). [int]")
+        # Survival and reproduction
+        self.death_bound = np.array(self.death_bound)
+        self.repr_bound = np.array(self.repr_bound)
+        if self.repr_mode in ["sexual", "assort_only"]: 
+            # Double fertility in sexual case to control for relative
+            # contribution to offspring
+            self.repr_bound *= 2 
         self.put("surv_bound", 1-self.death_bound[::-1], "Min and max \
                 survival rates. [float array].")
-        self.put("surv_step", np.diff(self.surv_bound)/self.n_states,
+        self.put("surv_step", np.diff(self.surv_bound)/(self.n_states-1),
                 "Difference in survival probability between adjacent \
                         reproduction genotype values. [float]")
-        self.put("repr_step", np.diff(self.repr_bound)/self.n_states,
+        self.put("repr_step", np.diff(self.repr_bound)/(self.n_states-1),
                 "Difference in reproduction probability between adjacent \
                         reproduction genotype values. [float]")
         self.put("s_range", np.linspace(self.surv_bound[0],self.surv_bound[1],
@@ -142,24 +174,23 @@ class Config:
         self.put("r_range", np.linspace(self.repr_bound[0],self.repr_bound[1],
             self.n_states), "Reproduction probability for each genotype sum \
                     value, linearly spaced. [float array]")
+        # Snapshot stages
         self.put("snapshot_stages", np.around(
             np.linspace(0,self.number_of_stages-1,self.number_of_snapshots),
             0).astype(int), "Stages of a run at which to record detailed \
                     information about the state of the population. \
                     [int array]")
+        # Params dict
         self.put("params", {
-            "sexual":self.sexual, "chr_len":self.chr_len, "n_base":self.n_base,
-            "maturity":self.maturity, "max_ls":self.max_ls, 
-            "start_pop":self.start_pop, "g_dist":self.g_dist
+            "repr_mode":self.repr_mode, "chr_len":self.chr_len,
+            "n_base":self.n_base, "maturity":self.maturity,
+            "max_ls":self.max_ls, "start_pop":self.start_pop,
+            "g_dist":self.g_dist
             }, "Key information for generating a new population object: \
                     reproduction mode, chromosome length, bases per \
                     chromosome, age of maturity, maximum lifespan, \
                     starting population size, and initial bit value \
                     distribution. [dict]")
-        if self.repr_mode in ["sexual", "assort_only"]: 
-            # Double fertility in sexual case to control for relative
-            # contribution to offspring
-            self.repr_bound *= 2 
 
         #! Script for generating new config file with default values,
         # or data file with default setup in place
---
+++ aegis/aegis/Core/Test/test_Config.py
@@ -0,0 +1,177 @@
+from aegis.Core import Config
+import pytest,importlib,types,random,copy,string
+import numpy as np
+
+@pytest.fixture(params=["import", "random", "random"])
+def conf(request):
+    """Create a default configuration object."""
+    c_import = importlib.import_module("config_test")
+    c = Config(c_import)
+    c.setup = request.param
+    if request.param == "random": # Randomise config parameters
+        # Run parameters
+        c.number_of_runs = random.randint(1,5)
+        c.number_of_stages = random.randint(50,400)
+        c.number_of_snapshots = random.randint(1,20)
+        c.start_pop = random.randint(200,1000)
+        # Reproductive mode
+        c.repr_mode = random.choice(
+                ['sexual','asexual','assort_only','recombine_only'])
+        # Death and reproduction parameters
+        db_low, rb_low = np.random.uniform(size=2)
+        db_high = db_low + random.uniform(0, 1-db_low)
+        rb_high = rb_low + random.uniform(0, 1-rb_low)
+        c.death_bound = np.array([db_low, db_high])
+        c.repr_bound = np.array([rb_low, rb_high])
+        # Mutation and recombination
+        c.r_rate, c.m_rate, c.m_ratio = [random.random() for x in range(3)]
+        # Genome structure
+        c.g_dist_s,c.g_dist_r,c.g_dist_n = [random.random() for x in range(3)]
+        c.n_neutral = random.randint(1, 100)
+        c.n_base = random.randint(5, 25)
+        c.repr_offset = random.randint(80,180)
+        c.neut_offset = random.randint(c.repr_offset*3,c.repr_offset*5)
+        # Life histories
+        c.max_ls = random.randint(20, c.repr_offset-1)
+        c.maturity = random.randint(5, c.max_ls-2)
+        # Resources and starvation
+        c.res_start = random.randint(500,2000)
+        c.res_var = random.choice([True, False])
+        c.res_limit = random.randint(3000,10000)
+        c.V = random.randrange(10)/10.0 + random.randint(1,3)
+        c.R = random.randint(500,2000)
+        c.surv_pen = random.choice([True, False])
+        c.repr_pen = random.choice([True, False])
+        c.death_inc = random.randint(1, 10)
+        c.repr_dec = random.randint(1, 10)
+        gm_len = c.max_ls + (c.max_ls - c.maturity) + c.n_neutral
+        # Sliding windows
+        c.windows = {
+                "population_size": random.randint(500,1500),
+                "resources": random.randint(500,1500),
+                "n1": random.randint(5,20)
+                }
+    # Generate derived parameters
+    c.generate()
+    return c
+
+class TestConfig:
+
+    def test_config_init(self, conf):
+        """Test that Config objects are correctly initialised from an imported
+        configuration file."""
+        if conf.setup == "random": return
+        c = copy.deepcopy(conf)
+        # Remove stuff that gets introduced/changed during generation
+        del c.g_dist, c.genmap, c.chr_len, c.s_range, c.r_range, c.params
+        del c.snapshot_stages, c.surv_step, c.repr_step, c.genmap_argsort
+        del c.n_states, c.surv_bound
+        if c.repr_mode in ["sexual", "assort_only"]: c.repr_bound /= 2
+        # Compare remaining keys to directly imported config file
+        c_import = importlib.import_module("config_test")
+        for key in c.__dict__.keys():
+            if key in ["setup", "info_dict"]: continue
+            assert type(c.get_info(key)) is str
+            assert len(c.get_info(key)) > 0
+            attr = c.get_value(key)
+            impt = getattr(c_import,key)
+            if type(attr) in [int, str, float, dict, bool]:
+                assert attr == impt
+            elif type(attr) is np.ndarray:
+                assert np.array_equal(attr, impt)
+            else:
+                errstr = "Unexpected config element type: "
+                raise TypeError(errstr + str(type(attr)))
+
+    def test_config_check(self,conf):
+        """Test that configurations with incompatible genome parameters are
+        correctly rejected."""
+        if conf.setup == "random": return
+        c = copy.deepcopy(conf)
+        c.check()
+        c.maturity = c.max_ls - 1
+        with pytest.raises(ValueError):
+            c.check()
+        c.maturity += 1
+        c.repr_offset = c.max_ls - 1
+        with pytest.raises(ValueError):
+            c.check()
+        c.repr_offset += 1
+        c.neut_offset = c.repr_offset + c.max_ls - 1
+        with pytest.raises(ValueError):
+            c.check()
+
+    def test_config_getput(self,conf):
+        """Test constructors and selectors for Config object."""
+        if conf.setup == "random": return
+        # Set up random inputs
+        s = string.ascii_lowercase
+        keystr = ''.join(random.choice(s) for _ in xrange(10))
+        valstr = ''.join(random.choice(s) for _ in xrange(10))
+        infostr = ''.join(random.choice(s) for _ in xrange(10))
+        # Perform tests
+        c = copy.deepcopy(conf)
+        c.put(keystr, valstr, infostr)
+        assert c.get_value(keystr) == valstr
+        assert c.get_info(keystr) == infostr
+
+    def test_config_generate(self, conf):
+        """Test that gen_conf correctly generates derived simulation params."""
+        c = copy.deepcopy(conf)
+        np.set_printoptions(threshold=np.nan)
+        # Remove stuff that gets introduced/changed during generation
+        del c.g_dist, c.genmap, c.chr_len, c.s_range, c.r_range, c.params
+        del c.snapshot_stages, c.surv_step, c.repr_step, c.genmap_argsort
+        del c.n_states, c.surv_bound
+        sexvar = c.repr_mode in ["sexual", "assort_only"]
+        if sexvar: c.repr_bound /= 2
+        # Save info and run
+        crb1 = c.repr_bound[1]
+        c.generate()
+        # Test output:
+        # Genome structure
+        print c.max_ls, c.maturity, c.repr_offset, c.neut_offset, c.n_neutral
+        print c.neut_offset - c.repr_offset
+        print c.neut_offset - c.repr_offset < c.max_ls
+        print c.genmap
+        assert c.g_dist == {"s":c.g_dist_s, "r":c.g_dist_r, "n":c.g_dist_n}
+        assert len(c.genmap) == c.max_ls + (c.max_ls-c.maturity) +\
+                c.n_neutral
+        assert np.sum(c.genmap < c.repr_offset) == c.max_ls
+        assert np.sum(np.logical_and(
+            c.genmap >= c.repr_offset,c.genmap < c.neut_offset
+            )) == c.max_ls - c.maturity
+        assert np.sum(c.genmap >= c.neut_offset) == c.n_neutral
+        assert np.array_equal(np.argsort(c.genmap), c.genmap_argsort)
+        assert c.chr_len == len(c.genmap) * c.n_base
+        assert c.n_states ==  2*c.n_base+1
+        # Survival and reproduction
+        assert c.repr_bound[1]/crb1 == 2 if sexvar else 1
+        assert np.array_equal(c.surv_bound,1-c.death_bound[::-1])
+        assert np.array_equal(c.s_range, 
+                np.linspace(c.surv_bound[0],c.surv_bound[1],
+                    c.n_states))
+        assert np.array_equal(c.r_range, 
+                np.linspace(c.repr_bound[0],c.repr_bound[1],
+                    c.n_states))
+        assert c.surv_step == np.diff(c.surv_bound)/(c.n_states-1)
+        assert c.repr_step == np.diff(c.repr_bound)/(c.n_states-1)
+        print c.s_range
+        print np.diff(c.s_range)
+        print c.r_range
+        print np.diff(c.r_range)
+        assert np.allclose(np.diff(c.s_range), c.surv_step)
+        assert np.allclose(np.diff(c.r_range), c.repr_step)
+        # Snapshot stages
+        assert len(c.snapshot_stages) == c.number_of_snapshots
+        assert np.array_equal(c.snapshot_stages,np.around(np.linspace(
+            0, c.number_of_stages-1, c.number_of_snapshots), 0))
+        # Params dict
+        assert type(c.params) is dict
+        assert c.params["repr_mode"] == c.repr_mode
+        assert c.params["chr_len"] == c.chr_len
+        assert c.params["n_base"] == c.n_base
+        assert c.params["maturity"] == c.maturity
+        assert c.params["max_ls"] == c.max_ls
+        assert c.params["start_pop"] == c.start_pop
+        assert c.params["g_dist"] == c.g_dist
