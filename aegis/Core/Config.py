Change autostage computation. Fix what happens when n_stage exceeds max_stages.

aegis/Core/Config.py
@@ -119,12 +119,21 @@ class Config(dict):
     def autostage(self):
         """Compute automatic running behaviour ... UNTESTED"""
         if self["auto"]:
+            # raise error if res not constant since not implemented
+            if self["res_function"](100,100)!=100:
+                s = "Autostage computing with non-constant resources is not\
+                        implemented."
+                raise ValueError(s)
             # Compute analytical parameters
             alpha, beta = self["m_rate"], self["m_rate"]*self["m_ratio"]
+            zeta = self["zeta"]
             y = self["g_dist"]["n"]
             x = 1-y
-            k = math.log10(self["delta"]*(alpha+beta)/abs(alpha*y-beta*x)) / \
-                    math.log10(abs(1-alpha-beta))
+            ssize = self["res_start"] * self["chr_len"] * 2 # sample size
+            epsbar = np.sqrt(1.0/(2*ssize)*np.log(2.0/zeta))
+            delta = epsbar * 0.1
+            k = np.log(delta*(alpha+beta)/abs(alpha*y-beta*x)) / \
+                    np.log(abs(1-alpha-beta))
             # Assign generation threshold
             self["min_gen"] = int(k*self["scale"])
 

aegis/Core/Record.py
@@ -439,7 +439,7 @@ class Record(dict):
     def finalise(self):
         """Calculate additional stats from recorded data of a completed run."""
         # If dieoff, truncate data to last snapshot pop
-        if self["dieoff"]:
+        if self["dieoff"] or (self["auto"] and self["population_size"][-1]):
             pops = np.array(self["snapshot_pops"])
             pops = pops[np.nonzero(pops)]
             self["snapshot_pops"] = list(pops)

aegis/Core/Test/test_1_Config.py
@@ -54,7 +54,8 @@ def conf(request, conf_path, ran_str):
         ## RESOURCE PARAMETERS ##
         c["res_limit"] = random.randint(5000,10000)
         a,b,d,e = np.random.uniform(size=4)
-        c["res_function"] = lambda n,r: int((r-n)*a + b) # Random affine
+        # leave resources constant since autostage not implemented for non-constant
+        #c["res_function"] = lambda n,r: int((r-n)*a + b) # Random affine
         c["stv_function"] = lambda n,r: e*n > d*r
         ## AUTOCOMPUTING STAGE NUMBER ##
         c["delta"] = 10**-random.randint(5,15)
@@ -282,14 +283,15 @@ class TestConfig:
         assert c["params"]["g_dist"] == c["g_dist"]
         # Snapshot stages
         if c["auto"]:
-            #m,r = c["m_rate"], c["m_ratio"]
-            #A, P = abs(1 - m - m*r), abs(r/(1+r) - c["g_dist_n"])
-            #k = (math.log10(c["delta"]) - math.log10(P))/math.log10(A)
             alpha, beta = c["m_rate"], c["m_rate"]*c["m_ratio"]
+            zeta = c["zeta"]
             y = c["g_dist"]["n"]
             x = 1-y
-            k = math.log10(c["delta"]*(alpha+beta)/abs(alpha*y-beta*x)) / \
-                    math.log10(abs(1-alpha-beta))
+            ssize = c["res_start"] * c["chr_len"] * 2
+            epsbar = np.sqrt(1.0/(2*ssize)*np.log(2.0/zeta))
+            delta = epsbar * 0.1
+            k = np.log(delta*(alpha+beta)/abs(alpha*y-beta*x)) / \
+                    np.log(abs(1-alpha-beta))
             assert c["min_gen"] == int(k * c["scale"])
             assert len(c["snapshot_generations"]) == c["n_snapshots"]
             assert len(c["snapshot_generations"]) == c["n_snapshots"]

aegis/Core/Test/test_2a_Population_init.py
@@ -302,11 +302,11 @@ class TestPopulationInit:
         """Test that genome array is of the correct size and that
         the loci are distributed correctly."""
         precision = 0.05
-        # Set up population
-        pop2 = pop.clone()
-        pop2.N = 1000
         # Define testing function
         def test_mga(genmap):
+            # Set up population
+            pop2 = pop.clone()
+            pop2.N = 1000
             loci = {
                 "s":np.nonzero(genmap<pop2.repr_offset)[0],
                 "r":np.nonzero(np.logical_and(genmap>=pop2.repr_offset,
@@ -326,8 +326,8 @@ class TestPopulationInit:
                 assert tstat < precision
         # First with simple linear genmap, then with shuffled form
         genmap1 = np.concatenate((np.arange(25),
-            np.arange(24) + pop2.repr_offset,
-            np.arange(5) + pop2.neut_offset), 0)
+            np.arange(24) + pop.repr_offset,
+            np.arange(5) + pop.neut_offset), 0)
         genmap2 = np.copy(genmap1)
         random.shuffle(genmap2)
         test_mga(genmap1)

aegis/Core/Test/test_3_Record.py
@@ -717,7 +717,7 @@ class TestRecord:
         assert np.array_equal(R["age_dist_stages"], np.array([range(10)]))
         # general case
         m_snap = np.random.randint(2,20)
-        ix = np.random.randint(0,np.random.randint(1000,10000),m_snap)
+        ix = np.random.choice(np.random.randint(1000,10000),m_snap,replace=False)
         ix.sort()
         ads = [range(ix[2*i],ix[2*i+1]) for i in range(ix.shape[0]/2)]
         minl = ix[1] - ix[0]