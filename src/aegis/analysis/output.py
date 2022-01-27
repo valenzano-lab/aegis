99bd5136dabf54c4df2e3db3047a4c8c3ae3abf3
Martin Bagic <martin.bagic@outlook.com>
2022-01-27 19:24:03 +0100

    Add Output class

---
+++ src/aegis/analysis/output.py
@@ -0,0 +1,45 @@
+import pathlib
+import pandas as pd
+import json
+
+
+class Output:
+    def __init__(self, path):
+        self.path = pathlib.Path(path)
+
+        ascending = lambda s: int(s.stem)
+
+        ### SAVE FILE PATHS
+
+        # visor
+        self.visor_paths = list((self.path / "0/visor").glob("*.csv"))
+        self.visor_spectra_paths = list((self.path / "0/visor/spectra").glob("*.csv"))
+
+        # popgen
+        self.popgen_paths = list((self.path / "0/popgen").glob("*"))
+
+        # pickles
+        self.pickle_paths = sorted((self.path / "0/pickles").glob("*"), key=ascending)
+
+        # snapshots
+        self.demography_paths = sorted(
+            (self.path / "0/snapshots/demography").glob("*"), key=ascending
+        )
+        self.genotypes_paths = sorted(
+            (self.path / "0/snapshots/genotypes").glob("*"), key=ascending
+        )
+        self.phenotypes_paths = sorted(
+            (self.path / "0/snapshots/phenotypes").glob("*"), key=ascending
+        )
+
+        ### READ FILES
+
+        for path in self.visor_paths + self.visor_spectra_paths + self.popgen_paths:
+            csv = pd.read_csv(path, header=None)
+            attr = path.stem
+            setattr(self, attr, csv)
+
+        # other
+        with open(self.path / "0/output_summary.json", "r") as file_:
+            self.output_summary = json.load(file_)
+        self.progresslog = pd.read_csv(self.path / "progress.log", sep="|")
