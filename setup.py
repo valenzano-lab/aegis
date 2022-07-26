1408a46971d096e55f9c2ee9ad613af6bb12c1aa
Martin Bagic <martin.bagic@outlook.com>
2022-07-26 10:19:16 +0200

    Bump up version to 2.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import pathlib
 
 setuptools.setup(
     name="aegis-sim",
-    version="2.0",
+    version="2.1",
     description="Numerical model for life history evolution of age-structured populations",
     long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     author="Martin Bagic, Dario Valenzano",
-    author_email="martin.bagic@outlook.com, Dario.Valenzano@age.mpg.de",
+    author_email="martin.bagic@outlook.com, Dario.Valenzano@leibniz-fli.de",
     url="https://github.com/valenzano-lab/aegis",
     package_dir={"": "src"},
     packages=[
