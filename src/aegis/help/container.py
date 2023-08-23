e7f3b72a23eae72a6ac42e0631d91707049647de
Martin Bagic <martin.bagic@outlook.com>
2023-08-23 13:06:01 +0200

    Make Container load log

--- src/aegis/help/container.py
+++ src/aegis/help/container.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pathlib
+import logging
 
 
 class Container:
@@ -9,13 +10,30 @@ class Container:
     """
 
     def __init__(self, basepath):
-        self.basepath = pathlib.Path(basepath)
+        self.basepath = pathlib.Path(basepath).absolute()
         self.paths = {
             path.stem: path
             for path in self.basepath.glob("**/*")
             if path.is_file() and path.suffix == ".csv"
         }
+        self.paths["log"] = self.basepath / "progress.log"
         self.data = {}
+
+        if not self.paths["log"].is_file():
+            logging.error(f"No AEGIS log found at path {self.paths['log']} ")
+
+    def get_log(self):
+        if "log" not in self.data:
+            df = pd.read_csv(self.paths["log"], sep="|")
+            df.columns = [x.strip() for x in df.columns]
+
+            def dhm_inverse(dhm):
+                nums = dhm.replace("`", ":").split(":")
+                return int(nums[0]) * 24 * 60 + int(nums[1]) * 60 + int(nums[2])
+
+            df[["ETA", "t1M", "runtime"]].applymap(dhm_inverse)
+            self.data["log"] = df
+        return self.data["log"]
 
     def get_df(self, stem):
         file_read = stem in self.data
