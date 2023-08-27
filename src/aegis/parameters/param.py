4787df6b21c00fe4a330c3809b6f821917df1d8d
Martin Bagic <martin.bagic@outlook.com>
2023-08-27 03:45:31 +0200

    Add inrange attribute to parameters

--- src/aegis/parameters/param.py
+++ src/aegis/parameters/param.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Param:
     def __init__(self, name, default, info, dtype, drange):
 
@@ -13,6 +16,21 @@ class Param:
         else:
             return self.dtype(value)
 
+    def valid(self, value):
+        # Not valid if wrong data type
+        if not isinstance(value, self.dtype):
+            logging.error(
+                f"Value {value} is not of valid type {self.dtype} but of type {type(value)}"
+            )
+            return False
+
+        # Not valid if not in range
+        if not self.inrange(value):
+            return False
+
+        # Valid
+        return True
+
 
 params = {
     "RANDOM_SEED_": Param(
@@ -21,6 +39,7 @@ params = {
         info="If nothing is given, a random integer will be used as the seed; otherwise the given integer will be used as the seed",
         dtype=int,
         drange="int, none",
+        inrange=lambda x: True,
     ),
     "STAGES_PER_SIMULATION_": Param(
         name="STAGES_PER_SIMULATION_",
@@ -28,6 +47,7 @@ params = {
         info="How many stages does the simulation run for?",
         dtype=int,
         drange="1+",
+        inrange=lambda x: x >= 1,
     ),
     "LOGGING_RATE_": Param(
         name="LOGGING_RATE_",
@@ -35,6 +55,7 @@ params = {
         info="Log every ?-th stage; 0 for no logging",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "PICKLE_RATE_": Param(
         name="PICKLE_RATE_",
@@ -42,6 +63,7 @@ params = {
         info="Pickle population every ? stages; 0 for no pickles",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "SNAPSHOT_RATE_": Param(
         name="SNAPSHOT_RATE_",
@@ -49,6 +71,7 @@ params = {
         info="Take a snapshot every ? stages; 0 for no snapshots",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "VISOR_RATE_": Param(
         name="VISOR_RATE_",
@@ -56,6 +79,7 @@ params = {
         info="Take a visor snapshot every ? stages; 0 for no visor records",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "POPGENSTATS_RATE_": Param(
         name="POPGENSTATS_RATE_",
@@ -63,6 +87,7 @@ params = {
         info="Record population genetic stats about the population every ? stages; 0 for no popgen stats",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "POPGENSTATS_SAMPLE_SIZE_": Param(
         name="POPGENSTATS_SAMPLE_SIZE_",
@@ -70,6 +95,7 @@ params = {
         info="Number of individuals to use when calculating population genetic metrics",
         dtype=int,
         drange="0, 3+",
+        inrange=lambda x: x == 0 or x >= 3,
     ),
     "ECOSYSTEM_NUMBER_": Param(
         name="ECOSYSTEM_NUMBER_",
@@ -77,6 +103,7 @@ params = {
         info="",
         dtype=int,
         drange="",
+        inrange=lambda x: x >= 1,
     ),
     "MAX_POPULATION_SIZE": Param(
         name="MAX_POPULATION_SIZE",
@@ -84,6 +111,7 @@ params = {
         info="Number of individuals in the population",
         dtype=int,
         drange="1+",
+        inrange=lambda x: x >= 1,
     ),
     "OVERSHOOT_EVENT": Param(
         name="OVERSHOOT_EVENT",
@@ -91,6 +119,14 @@ params = {
         info="Who dies when everyone is starving?",
         dtype=str,
         drange="starvation, cliff, treadmill_random, treadmill_zoomer, treadmill_boomer",
+        inrange=lambda x: x
+        in (
+            "starvation",
+            "cliff",
+            "treadmill_random",
+            "treadmill_zoomer",
+            "treadmill_boomer",
+        ),
     ),
     "CLIFF_SURVIVORSHIP": Param(
         name="CLIFF_SURVIVORSHIP",
@@ -98,6 +134,7 @@ params = {
         info="What fraction of population survives after a cliff?; null if not applicable",
         dtype=int,
         drange="null or 0-1 excluding 0 and 1",
+        inrange=lambda x: x is None or (0 < x < 1),
     ),
     "STAGES_PER_SEASON": Param(
         name="STAGES_PER_SEASON",
@@ -105,6 +142,7 @@ params = {
         info="How many stages does one season last; 0 for no seasons",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "MAX_LIFESPAN": Param(
         name="MAX_LIFESPAN",
@@ -112,6 +150,7 @@ params = {
         info="Maximum lifespan",
         dtype=int,
         drange="1+",
+        inrange=lambda x: x >= 1,
     ),
     "MATURATION_AGE": Param(
         name="MATURATION_AGE",
@@ -119,6 +158,7 @@ params = {
         info="Age at which reproduction is possible",
         dtype=int,
         drange="1+",
+        inrange=lambda x: x >= 1,
     ),
     "BITS_PER_LOCUS": Param(
         name="BITS_PER_LOCUS",
@@ -126,6 +166,7 @@ params = {
         info="Number of bits that each locus has",
         dtype=int,
         drange="1+",
+        inrange=lambda x: x >= 1,
     ),
     "HEADSUP": Param(
         name="HEADSUP",
@@ -133,6 +174,7 @@ params = {
         info="-1 if no preevolution, 0 for maturity guarantee, +x for headsup",
         dtype=int,
         drange="-1, 0, 1+",
+        inrange=lambda x: x in (-1, 0) or x >= 1,
     ),
     "REPRODUCTION_MODE": Param(
         name="REPRODUCTION_MODE",
@@ -140,6 +182,7 @@ params = {
         info="Mode of reproduction",
         dtype=str,
         drange="sexual, asexual, asexual_diploid",
+        inrange=lambda x: x in ("sexual", "asexual", "asexual_diploid"),
     ),
     "RECOMBINATION_RATE": Param(
         name="RECOMBINATION_RATE",
@@ -147,6 +190,7 @@ params = {
         info="Rate of recombination; 0 if no recombination",
         dtype=float,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "MUTATION_RATIO": Param(
         name="MUTATION_RATIO",
@@ -154,6 +198,7 @@ params = {
         info="Ratio of 0->1 mutations to 1->0 mutations",
         dtype=float,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "MUTATION_METHOD": Param(
         name="MUTATION_METHOD",
@@ -161,6 +206,7 @@ params = {
         info="",
         dtype=str,
         drange="by_bit, by_index",
+        inrange=lambda x: x in ("by_bit", "by_index"),
     ),
     "PHENOMAP_METHOD": Param(
         name="PHENOMAP_METHOD",
@@ -168,6 +214,7 @@ params = {
         info="Non-vectorized, vectorized and null method of calculating phenotypic values",
         dtype=str,
         drange="by_loop, by_dot, by_dummy",
+        inrange=lambda x: x in ("by_loop", "by_dot", "by_dummy"),
     ),
     "ENVIRONMENT_CHANGE_RATE": Param(
         name="ENVIRONMENT_CHANGE_RATE",
@@ -175,6 +222,7 @@ params = {
         info="Environmental map changes every ? stages; if no environmental change",
         dtype=int,
         drange="0+",
+        inrange=lambda x: x >= 0,
     ),
     "G_surv_evolvable": Param(
         name="G_surv_evolvable",
@@ -182,6 +230,7 @@ params = {
         info="",
         dtype=bool,
         drange="",
+        inrange=lambda x: True,
     ),
     "G_surv_agespecific": Param(
         name="G_surv_agespecific",
@@ -189,6 +238,7 @@ params = {
         info="",
         dtype=bool,
         drange="",
+        inrange=lambda x: True,
     ),
     "G_surv_interpreter": Param(
         name="G_surv_interpreter",
