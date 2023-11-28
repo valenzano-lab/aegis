c0cd7821356335f0be4f12b25fd823a9e321445b
Martin Bagic <martin.bagic@outlook.com>
2023-11-28 14:59:53 +0100

    Do not run visor on import only

--- src/aegis/visor/visor.py
+++ src/aegis/visor/visor.py
@@ -1,13 +1,11 @@
-# Import packages
-from dash import Dash, html, dcc, callback, Output, Input, State
-from aegis.visor.layout import app_layout
-import aegis.visor.callbacks
-import aegis.visor.tab_config.callbacks_config
-import aegis.visor.tab_plot.callbacks_plot
-import aegis.visor.tab_list.callbacks_list
-
-
 def run():
+    from dash import Dash, html, dcc, callback, Output, Input, State
+    from aegis.visor.layout import app_layout
+    import aegis.visor.callbacks
+    import aegis.visor.tab_config.callbacks_config
+    import aegis.visor.tab_plot.callbacks_plot
+    import aegis.visor.tab_list.callbacks_list
+
     app = Dash(
         __name__,
         suppress_callback_exceptions=True,
