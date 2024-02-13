26e9e95b89c5fd030b20008b4336d8e73e8a374d
Martin Bagic <martin.bagic@outlook.com>
2024-02-13 14:09:32 +0100

    Add table functions to Container

--- src/aegis/help/container.py
+++ src/aegis/help/container.py
@@ -5,31 +5,120 @@ import json
 import yaml
 from aegis.help.config import get_default_parameters
 from aegis.modules.population import Population
+from aegis.help.config import causeofdeath_valid
 
 
 class Container:
     """Wrapper class
-
     Contains paths to output files which it can read and return.
     """
 
     def __init__(self, basepath):
         self.basepath = pathlib.Path(basepath).absolute()
         self.name = self.basepath.stem
+        self.data = {}
+
+        # Set paths
         self.paths = {
             path.stem: path for path in self.basepath.glob("**/*") if path.is_file() and path.suffix == ".csv"
         }
         self.paths["log"] = self.basepath / "progress.log"
         self.paths["output_summary"] = self.basepath / "output_summary.json"
         self.paths["input_summary"] = self.basepath / "input_summary.json"
-        self.paths["snapshots"] = self.basepath / "snapshots"
-        self.paths["pickles"] = self.basepath / "pickles"
-        self.data = {}
-
+        self.paths["snapshots"] = {}
+        for kind in ("demography", "phenotypes", "genotypes"):
+            self.paths["snapshots"][kind] = sorted(
+                (self.basepath / "snapshots" / kind).glob("*"),
+                key=lambda path: int(path.stem),
+            )
+        self.paths["pickles"] = sorted(
+            (self.basepath / "pickles").glob("*"),
+            key=lambda path: int(path.stem),
+        )
         if not self.paths["log"].is_file():
             logging.error(f"No AEGIS log found at path {self.paths['log']}")
 
-    def get_log(self, reload=False):
+    def __str__(self):
+        return self.name
+
+    def get_record_structure():
+        # TODO
+        return
+
+    def report(self):
+        """Report present and missing files"""
+        # TODO
+        return
+
+    ### INTERFACING FUNCTIONS ("API")
+
+    # Tables
+    def get_birth_table(self, record_type, record_index=None, normalize=False):
+        if record_type == "interval":
+            table = self._read_df("age_at_birth")
+            if normalize:
+                table = table.div(table.sum(1), axis=0)
+            table.index.names = ["interval"]
+            table.columns.names = ["age"]
+            table.columns = table.columns.astype(int)
+            return table
+        elif record_type == "snapshot":
+            raise Exception("Snapshot birth table not available")
+        else:
+            raise Exception(f"record_type must be interval or snapshot, not {record_type}")
+
+    def get_life_table(self, record_type, record_index=None, normalize=False):
+        if record_type == "interval":
+            table = self._read_df("additive_age_structure")
+            table.index.names = ["interval"]
+            table.columns.names = ["age"]
+            table.columns = table.columns.astype(int)
+            return table
+        elif record_type == "snapshot":
+            MAX_LIFESPAN = self.get_config()["MAX_LIFESPAN"]
+            table = (
+                self.get_demography(record_type, record_index)
+                .ages.value_counts()
+                .reindex(range(MAX_LIFESPAN), fill_value=0)
+            )
+            table.index.names = ["age"]
+            return table
+        else:
+            raise Exception(f"record_type must be interval or snapshot, not {record_type}")
+
+    def get_death_table(self, record_type, record_index=None, normalize=False):
+        if record_type == "interval":
+            table = (
+                pd.concat(
+                    {causeofdeath: self._read_df(f"age_at_{causeofdeath}") for causeofdeath in causeofdeath_valid}
+                )
+                .swaplevel()
+                .sort_index(level=0)
+            )
+            table.index.names = ["interval", "cause"]
+            table.columns.names = ["age"]
+            table.columns = table.columns.astype(int)
+            return table
+        elif record_type == "snapshot":
+            raise Exception("Snapshot death table not available")
+        else:
+            raise Exception(f"record_type must be interval or snapshot, not {record_type}")
+
+    # Metadata
+
+    def read_metadata(self, which, **kwargs):
+        if which == "log":
+            return self.get_log(kwargs["reload"])
+        elif which == "config":
+            return self.get_config()
+        elif which == "input_summary":
+            return self.get_input_summary()
+        elif which == "output_summary":
+            return self.get_output_summary()
+        else:
+            raise Exception(f"{which} is not valid; enter 'log', 'config', 'input_summary' or 'output_summary'")
+
+    def get_log(self, reload=True):
         if ("log" not in self.data) or reload:
             df = pd.read_csv(self.paths["log"], sep="|")
             df.columns = [x.strip() for x in df.columns]
@@ -46,7 +135,51 @@ class Container:
             self.data["log"] = df
         return self.data["log"]
 
-    def get_df(self, stem, reload=False):
+    def get_config(self):
+        if "config" not in self.data:
+            path = self.basepath.parent / f"{self.basepath.stem}.yml"
+            with open(path, "r") as file_:
+                custom_config = yaml.safe_load(file_)
+            default_config = get_default_parameters()
+            self.data["config"] = {**default_config, **custom_config}
+
+        return self.data["config"]
+
+    def get_output_summary(self):
+        return self._read_json(self.paths["output_summary"])
+
+    def get_input_summary(self):
+        return self._read_json(self.paths["input_summary"])
+
+    # BASIC INPUTS
+    # Data
+    def get_genotypes(self, record_type, record_index=None, reload=True):
+        if record_type == "interval":
+            return self._read_df("genotypes", reload=reload)
+        elif record_type == "snapshot":
+            return self._read_snapshot("genotypes", record_index=record_index)
+        else:
+            raise Exception(f"record_type must be interval or snapshot, not {record_type}")
+
+    def get_phenotypes(self, record_type, record_index=None, reload=True):
+        if record_type == "interval":
+            return self._read_df("phenotypes", reload=reload)
+        elif record_type == "snapshot":
+            return self._read_snapshot("phenotypes", record_index=record_index)
+        else:
+            raise Exception(f"record_type must be interval or snapshot, not {record_type}")
+
+    def get_demography(self, record_type, record_index=None):
+        if record_type == "interval":
+            raise Exception("Interval demography not available")
+        elif record_type == "snapshot":
+            return self._read_snapshot("demography", record_index=record_index)
+        else:
+            raise Exception(f"record_type must be interval or snapshot, not {record_type}")
+
+    ### FORMAT READING FUNCTIONS
+
+    def _read_df(self, stem, reload=True):
         file_read = stem in self.data
         file_exists = stem in self.paths
         # TODO Read also files that are not .csv
@@ -58,57 +191,52 @@ class Container:
 
         return self.data.get(stem, pd.DataFrame())
 
-    def get_config(self):
-        if "config" not in self.data:
-            path = self.basepath.parent / f"{self.basepath.stem}.yml"
-            with open(path, "r") as file_:
-                custom_config = yaml.safe_load(file_)
-            default_config = get_default_parameters()
-            self.data["config"] = {**default_config, **custom_config}
+    # def _return_json(self, stem):
+    #     df = self._read_df(stem)
+    #     json = df.T.to_json(index=False, orient="split")
+    #     return json
 
-        return self.data["config"]
+    # def _return_json(self):
+    #     return json.dumps(self)
 
-    def get_json(self, stem):
-        df = self.get_df(stem)
-        json = df.T.to_json(index=False, orient="split")
-        return json
+    @staticmethod
+    def _read_json(path):
+        assert path.exists(), f"'{path}' does not exist."
+        with open(path, "r") as file_:
+            return json.load(file_)
 
-    def get_output_summary(self):
-        if self.paths["output_summary"].exists():
-            with open(self.paths["output_summary"], "r") as file_:
-                return json.load(file_)
+    def _read_snapshot(self, record_type, record_index):
+        assert record_type in self.paths["snapshots"], f"No records of '{record_type}' can be found in snapshots"
+        assert record_index < len(self.paths["snapshots"][record_type]), "Index out of range"
+        return pd.read_feather(self.paths["snapshots"][record_type][record_index])
 
-    def get_input_summary(self):
-        if self.paths["input_summary"].exists():
-            with open(self.paths["input_summary"], "r") as file_:
-                return json.load(file_)
+    def _read_pickle(self, record_index):
+        assert record_index < len(self.paths["pickles"]), "Index out of range"
+        return Population.load_pickle_from(self.paths["pickles"])
 
-    def get_snapshot(self, kind, index):
+    ### ANALYSIS FUNCTIONS
 
-        assert kind in (
-            "demography",
-            "phenotypes",
-            "genotypes",
-        ), f'kind can only be "demography", "phenotypes" or "genotypes"; you entered {kind}'
+    def get_intrinsic_mortality(self):
+        phenotypes = self._read_df("phenotypes")
+        max_age = self.get_config()["MAX_LIFESPAN"]
+        # TODO Ensure that you are slicing the phenotype array at right places
+        pdf = phenotypes.iloc[:, :max_age]
+        y = 1 - pdf
+        return y
 
-        paths = sorted(
-            (self.paths["snapshots"] / kind).glob("*"),
-            key=lambda path: int(path.stem),
-        )
+    def get_intrinsic_survivorship(self):
+        phenotypes = self._read_df("phenotypes")
+        max_age = self.get_config()["MAX_LIFESPAN"]
+        # TODO Ensure that you are slicing the phenotype array at right places
+        pdf = phenotypes.iloc[:, :max_age]
+        y = pdf.cumprod(axis=1)
+        return y
 
-        if index < len(paths):
-            return pd.read_feather(paths[index])
-
-    def __str__(self):
-        return self.name
-
-    def get_json(self):
-        return json.dumps(self)
-
-    def read_pickle(self, index):
-        paths = sorted(
-            (self.paths["pickles"]).glob("*"),
-            key=lambda path: int(path.stem),
-        )
-        assert index < len(paths), "Index out of range"
-        return Population.load_pickle_from(paths[index])
+    def get_intrinsic_fertility(self):
+        phenotypes = self._read_df("phenotypes")
+        max_age = self.get_config()["MAX_LIFESPAN"]
+        # TODO Ensure that you are slicing the phenotype array at right places
+        # TODO Ensure that fertility is 0 before maturity
+        fertility = phenotypes.iloc[:, max_age:]
+        y = fertility
+        return y
