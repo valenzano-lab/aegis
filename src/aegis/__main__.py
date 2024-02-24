e4e2d64c752abd83e278fb5c5699a17fb8dffe67
Martin Bagic <martin.bagic@outlook.com>
2024-02-24 14:05:49 +0100

    Relocate parse_arg

--- src/aegis/__main__.py
+++ src/aegis/__main__.py
@@ -9,6 +9,44 @@ logging.basicConfig(
 )
 
 
+def parse():
+    import argparse
+    import pathlib
+
+    # Define parser
+    parser = argparse.ArgumentParser(description="Aging of Evolving Genomes in Silico")
+    parser.add_argument(
+        "-c",
+        "--config_path",
+        type=str,
+        help="path to config file",
+        default="",
+    )
+    parser.add_argument(
+        "-p",
+        "--pickle_path",
+        type=str,
+        help="path to pickle file",
+        default="",
+    )
+    parser.add_argument(
+        "--overwrite",
+        "-o",
+        action="store_true",
+        help="overwrite old data with new simulation",
+        default=False,
+    )
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Decision tree
+    config_path = pathlib.Path(args.config_path).absolute() if args.config_path else ""
+    pickle_path = pathlib.Path(args.pickle_path).absolute() if args.pickle_path else ""
+
+    return config_path, pickle_path, args.overwrite
+
+
 def run_sim(config_path, pickle_path, overwrite):
 
     from aegis import pan
@@ -46,7 +84,6 @@ def run_sim(config_path, pickle_path, overwrite):
 
 
 if __name__ == "__main__":
-    from aegis.parse_args import parse
 
     config_path, pickle_path, overwrite = parse()
     if config_path == "":
