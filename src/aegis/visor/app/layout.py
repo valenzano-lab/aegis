7b00f0cdec5031f7a6d8c9ee4dd4f929569c3488
Martin Bagic <martin.bagic@outlook.com>
2024-07-24 09:37:36 +0200

    Add navbar to GUI

--- src/aegis/visor/app/layout.py
+++ src/aegis/visor/app/layout.py
@@ -3,35 +3,17 @@ from dash import html, dcc
 
 
 app_layout = html.Div(
-    id="main-container",
+    id="body-container",
     children=[
         dcc.Location(id="main-url", refresh=False),
         # checkers
         # dcc.Interval(id="results-exist-interval", interval=1000, n_intervals=0),
         # dcc.Interval(id="process-monitor-interval", interval=1000, n_intervals=0),
         # TITLE SECTION
-        html.Div(
-            style={"padding": "1rem 0rem 2rem 0"},
+        html.Nav(
+            id="sidebar",
             children=[
-                html.Div(
-                    className="title-section",
-                    children=[
-                        html.Div(
-                            style={
-                                "display": "flex",
-                                "flex-wrap": "wrap",
-                                "margin-right": "2rem",
-                            },
-                            children=[
-                                html.H1(dcc.Link("aegis", href="/")),
-                            ],
-                        ),
-                    ],
-                ),
-            ],
-        ),
-        html.Div(
-            [
+                html.H1(dcc.Link("aegis", href="/")),
                 dcc.Link("home", href="/", id="link-nav-home"),
                 dcc.Link("config", href="config", id="link-nav-config"),
                 dcc.Link("plot", href="plot", id="link-nav-plot"),
@@ -39,14 +21,6 @@ app_layout = html.Div(
                 dcc.Link("wiki", href="wiki", id="link-nav-wiki"),
             ],
         ),
-        dash.page_container,
-        html.Hr(),
-        # FOOTER SECTION
-        html.Div(
-            [
-                # html.Hr(),
-                # html.A("github link", href="https://github.com/valenzano-lab/aegis", className="footer-text"),
-            ]
-        ),
+        html.Div(id="main-container", children=[dash.page_container]),
     ],
 )
