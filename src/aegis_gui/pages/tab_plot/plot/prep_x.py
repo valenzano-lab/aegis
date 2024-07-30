Fix step scaling in GUI graphs

src/aegis_gui/pages/tab_plot/plot/prep_x.py
@@ -9,6 +9,11 @@ def get_ages(container, **_):
     return np.arange(1, container.get_config()["AGE_LIMIT"] + 1)
 
 
-def get_steps(container, **kwargs):
+def get_steps_multiplied(container, **kwargs):
     y = kwargs["y"]
     return np.arange(1, len(y) + 1) * container.get_config()["INTERVAL_RATE"]
+
+
+def get_steps_non_multiplied(container, **kwargs):
+    y = kwargs["y"]
+    return np.arange(1, len(y) + 1)