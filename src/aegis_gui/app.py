701f68d6ff6efa736f34c58c045e9df7801998d8
Martin Bagic <martin.bagic@outlook.com>
2024-08-10 16:41:37 +0200

    Add progress toasts

--- src/aegis_gui/app.py
+++ src/aegis_gui/app.py
@@ -1,35 +1,8 @@
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-from aegis_gui.utilities import log_funcs, sim_tracker
-
-
-nav = dbc.Nav(
-    children=[
-        dbc.NavItem([dbc.NavLink([html.I(className="bi bi-house-door-fill"), "Home"], href="/", id="link-nav-home")]),
-        dbc.NavItem(
-            [
-                dbc.NavLink(
-                    [html.I(className="bi bi-rocket-takeoff-fill"), "Launch"], href="/config", id="link-nav-config"
-                )
-            ]
-        ),
-        dbc.NavItem(
-            [dbc.NavLink([html.I(className="bi bi-bar-chart-fill"), "Plot"], href="/plot", id="link-nav-plot")]
-        ),
-        dbc.NavItem(
-            [dbc.NavLink([html.I(className="bi bi-eye-fill"), "Control"], href="/simlog", id="link-nav-simlog")]
-        ),
-        dbc.NavItem(
-            [dbc.NavLink([html.I(className="bi bi-info-square-fill"), "Wiki"], href="/wiki", id="link-nav-wiki")]
-        ),
-    ]
-    + sim_tracker.make_tracker_component(),
-    id="sidebar",
-    vertical="md",
-    pills=True,
-    # fill=True,
-)
+from aegis_gui.utilities import log_funcs
+from aegis_gui import sidebar
 
 
 @dash.callback(
@@ -60,7 +33,7 @@ app_layout = html.Div(
         #     children=[Item([dbc.NavLink("wiki", href="wiki", id="link-nav-wiki"),
         #     ],
         # ),
-        nav,
+        sidebar.nav,
         html.Div(id="main-container", children=[dash.page_container]),
     ],
 )
--- src/aegis_gui/utilities/sim_tracker.py
+++ src/aegis_gui/utilities/sim_tracker.py
@@ -6,28 +6,53 @@ import dash_bootstrap_components as dbc
 
 def make_trackers():
     trackers = []
+
+    running_sims = []
+
     paths = utilities.get_sim_paths()
     for path in paths:
         container = Container(path)
         is_running = not container.has_ticker_stopped()
         if is_running:
-            tracker = dash.dcc.Link(
-                children=container.name, href=f"/simlog?sim={container.name}", className="sidebar-sim-running"
-            )
+            # tracker = dash.dcc.Link(
+            #     children=container.name, href=f"/simlog?sim={container.name}", className="sidebar-sim-running"
+            # )
+            # tracker = dbc.Button(
+            #     children=[container.name],
+            #     href=f"/simlog?sim={container.name}",
+            #     className="badge me-1 sidebar-sim-running",
+            #     color="primary",
+            # )
             progress = container.get_simple_log()
             if progress is None:
                 continue
             progress = progress[0] / progress[1]
             progressbar = dbc.Progress(
-                label=f"{progress*100:.0f}%" if progress > 0.1 else "",
+                # label=f"{progress*100:.0f}%" if progress > 0.1 else "",
+                label=container.name,
                 value=progress * 100,
                 striped=True,
                 animated=True,
             )
-            trackers.append(tracker)
-            trackers.append(progressbar)
+            linked_progressbar = dash.dcc.Link(
+                children=progressbar,
+                href=f"/simlog?sim={container.name}",
+            )
+            # running_sims.append(tracker)
+            running_sims.append(linked_progressbar)
 
-    trackers.append("Recent simulations:")
+    if running_sims:
+        trackers.append(
+            dbc.Toast(
+                children=[
+                    # dash.html.Hr(),
+                    # dash.html.P("Running simulations", className="text-secondary", style={"margin": 0}),
+                ]
+                + running_sims,
+                header="Running sims",
+                style={"width": "100%", "margin-top": "1rem"},
+            )
+        )
 
     recent_containers = []
 
@@ -36,28 +61,31 @@ def make_trackers():
         ticker = container.get_ticker()
         if ticker.since_last() < 3600 and container.has_ticker_stopped():
             recent_containers.append(container)
-            # trackers.append(
-            #     dash.dcc.Link(
-            #         children=container.name, href=f"/simlog?sim={container.name}", className="sidebar-sim-recent"
-            #     )
-            # )
 
-    nav = dash.html.Div(
-        children=[
-            dbc.Button(
-                children=[rc.name],
-                href=f"/simlog?sim={rc.name}",
-                # className="sidebar-sim-recent",
-                # outline=True,
-                className="badge me-1",
-                color="secondary",
-                # className="badge badge-pill badge-primary",
+    if recent_containers:
+        # trackers.append(dash.html.P("Recent simulations", className="text-secondary"))
+
+        nav = dash.html.Div(
+            children=[
+                dbc.Button(
+                    children=[rc.name],
+                    href=f"/simlog?sim={rc.name}",
+                    className="badge me-1",
+                    color="secondary",
+                )
+                for rc in recent_containers
+            ],
+        )
+
+        trackers.append(
+            dbc.Toast(
+                children=nav,
+                header="Recent sims",
+                style={"width": "100%", "margin-top": "1rem"},
             )
-            for rc in recent_containers
-        ],
-    )
+        )
 
-    trackers.append(nav)
+        # trackers.append(nav)
 
     return trackers
 
@@ -65,7 +93,7 @@ def make_trackers():
 def make_tracker_component():
     return [
         dash.dcc.Interval(id="running-simulations-interval", interval=1 * 1000, n_intervals=0),
-        dash.html.Div(["running-simulations"] + make_trackers(), id="running-simulations"),
+        dash.html.Div(make_trackers(), id="running-simulations"),
     ]
 
 
@@ -74,5 +102,4 @@ def make_tracker_component():
     dash.Input("running-simulations-interval", "n_intervals"),
 )
 def update_simulations(n):
-    # Call make_trackers() to get the updated list of elements
-    return ["running-simulations"] + make_trackers()
+    return make_trackers()
