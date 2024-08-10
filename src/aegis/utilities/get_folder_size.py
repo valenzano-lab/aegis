caa8405bc68a2d5d3cb897f3d07be9678cc617ae
Martin Bagic <martin.bagic@outlook.com>
2024-08-10 09:30:56 +0200

    Add bytes to folder size in simlog

--- src/aegis/utilities/get_folder_size.py
+++ src/aegis/utilities/get_folder_size.py
@@ -3,4 +3,4 @@ import subprocess
 
 def get_folder_size_with_du(folder_path):
     result = subprocess.run(["du", "-sh", folder_path], stdout=subprocess.PIPE, text=True)
-    return result.stdout.split()[0]
+    return result.stdout.split()[0] + "B"
