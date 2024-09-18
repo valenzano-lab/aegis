3bcb7c62dc78ef9718bd8fc6624b9efea57e1e91
Martin Bagic <martin.bagic@outlook.com>
2024-09-18 07:09:03 +0200

    Update parameter specification

--- src/aegis/documentation/dynamic/write.py
+++ src/aegis/documentation/dynamic/write.py
@@ -13,18 +13,44 @@ here = pathlib.Path(__file__).parent
 
 
 def get_default_parameter_dictlists():
-    cols = ["key", "name", "default", "info", "dtype", "drange"]
+    cols = [
+        "key",
+        # "name",
+        "default",
+        "info",
+        "dtype",
+        "drange",
+    ]
     dictlists = [{col: getattr(p, col) for col in cols} for p in DEFAULT_PARAMETERS.values()]
     return dictlists
 
 
-def write_dictlists_as_markdown_table(dict_list, path):
+parameter_specifications_preamble = """
+# Parameter specification
+
+There are three kinds of properties in AEGIS: fixed, parametrizable and variable.
+- Fixed properties, such as lack of mating preference, is hard-coded in the current version of AEGIS – it is valid for all simulations and all individuals and cannot be changed.
+- Parametrizable properties, such as reproduction mode, is parametrizable – it can be set by a parameter (`REPRODUCTION_MODE`) to various values including `sexual` and `asexual`.
+- All other properties, e.g. fertility (biological) or abiotic mortality (environmental), are variable – they differ between individuals and they vary over simulation time.  
+
+The table below provides information about all parameters in AEGIS. Here we provide an explanation of table columns.
+- `key` gives the parameter names as they are given in the GUI and as they are expected in the .yml input configuration files.
+- `default` gives the default value of the parameter. This is the value that will be used if the user does not provide any value.
+- `info` gives a short description of the parameter. Note that to understand some of the parameters it is necessary to understand how they modify the behavior of the submodel they affect.
+- `dtype` gives the data type of the parameter value.
+- `drange` specifies the valid range of values that can be used for the parameter.
+
+"""
+
+
+def write_dictlists_as_markdown_table(dict_list, path, preamble):
     # Requires tabulate package
     # dict_list like OUTPUT_SPECIFICATIONS; [{col1: val1, col2, val2, ...}, {col1: val1', col2: val2', ...}, ...]
     df = pd.DataFrame(dict_list)
     md = df.to_markdown()
     with open(path, "w") as file_:
-        file_.writelines(specifications.output_specifications_preamble)
+        if preamble:
+            file_.writelines(preamble)
         file_.writelines(md)
 
 
@@ -38,6 +64,17 @@ def dict_to_markdown(data_dict, output_file):
             md_file.write(f"{value}\n\n")  # Add double newline to separate paragraphs
 
 
-write_dictlists_as_markdown_table(get_default_parameter_dictlists(), path=here / "default_parameters.md")
-write_dictlists_as_markdown_table(specifications.output_specifications, path=here / "output_specifications.md")
-dict_to_markdown(TEXTS_DOMAIN_MD, output_file=here / "submodel_specifications.md")
+write_dictlists_as_markdown_table(
+    get_default_parameter_dictlists(),
+    path=here / "default_parameters.md",
+    preamble=parameter_specifications_preamble,
+)
+write_dictlists_as_markdown_table(
+    specifications.output_specifications,
+    path=here / "output_specifications.md",
+    preamble=specifications.output_specifications_preamble,
+)
+dict_to_markdown(
+    TEXTS_DOMAIN_MD,
+    output_file=here / "submodel_specifications.md",
+)
