66106181b1f753565959ce1bc1eabd35364f2d5b
Martin Bagic <martin.bagic@outlook.com>
2025-05-01 09:25:22 +0200

    Change numba logging level to ERROR

--- src/aegis/__init__.py
+++ src/aegis/__init__.py
@@ -1,6 +1,6 @@
 """
 This script is executed when AEGIS is imported (`import aegis`). Execute functions by running `aegis.run_from_{}`.
-AEGIS can be started in multiple ways; each of these functions starts AEGIS from a different context. 
+AEGIS can be started in multiple ways; each of these functions starts AEGIS from a different context.
 """
 
 import logging
@@ -16,6 +16,7 @@ def start_from_terminal():
     parser = get_parser()
     args = parser.parse_args()
     set_logging(level=logging.DEBUG)
+    logging.getLogger("numba").setLevel(logging.ERROR)
 
     if args.command == "sim":
         config_path = pathlib.Path(args.config_path).absolute() if args.config_path else None
